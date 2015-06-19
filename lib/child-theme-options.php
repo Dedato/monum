9b6e3b3a680a403b58803b28b2af08207b620ab1
Toine Kamps <t.kamps@dd-tkamps.local>
2015-06-19 17:30:24 +0200

    Added editor style

--- lib/child-theme-options.php
+++ lib/child-theme-options.php
@@ -1,54 +1,12 @@
 <?php
 /* ==========================================================================
-   LESS compiler
-   ========================================================================== */
-  
-function autoCompileLess() {
-  
-  // include lessc.inc
-  require_once( get_stylesheet_directory() .'/assets/less/lessc.inc.php' );
-  
-  /*$parentFiles = array(    
-    'input'   => get_template_directory().'/styles/monum.less',
-    'output'  => get_template_directory().'/styles/monum.css'
-  );*/
-  $childFiles = array(    
-    'input'   => get_stylesheet_directory().'/assets/less/main.less',
-    'output'  => get_stylesheet_directory().'/assets/css/main.css'
-  );
-  $compileFiles = array($childFiles);
-  
-  foreach ($compileFiles as $compileFile) {
-    
-    // load the cache
-    $cacheFile = $compileFile['input'].'.cache';
-    if (file_exists($cacheFile)) {
-      $cache = unserialize(file_get_contents($cacheFile));
-    } else {
-      $cache = $compileFile['input'];
-    }
-    
-    // create a new cache object, and compile
-    $less = new lessc;
-    $less->setFormatter('compressed');
-    try {
-      $newCache = $less->cachedCompile($cache);
-    } catch (Exception $ex) {
-      echo "lessphp fatal error: ".$ex->getMessage();
-    }
-
-    // output a LESS file, and cache file only if it has been modified since last compile
-    if (!is_array($cache) || $newCache['updated'] > $cache['updated']) {
-      file_put_contents($cacheFile, serialize($newCache));
-      file_put_contents($compileFile['output'], $newCache['compiled']);
-    }
-  }
-  
+   Roots setup
+   ========================================================================== */ 
+function roots_setup() {
+  // Tell the TinyMCE editor to use a custom stylesheet
+  add_editor_style('/assets/css/editor-style.css');
 }
-/* Only compile when logged in    
-if ( is_user_logged_in() ) {
-  add_action('init', 'autoCompileLess');
-}*/
+add_action('after_setup_theme', 'roots_setup');
 
 
 /* ==========================================================================
@@ -57,10 +15,6 @@ if ( is_user_logged_in() ) {
   
 /* Increase JPG compression */ 
 add_filter( 'jpeg_quality', create_function( '', 'return 100;' ) );
-
-/* Enqueue custom script */
-//wp_enqueue_script('monum_js', get_stylesheet_directory_uri() .'/assets/js/monum.js', array(), null, true);
-
 
 
 /* ==========================================================================
