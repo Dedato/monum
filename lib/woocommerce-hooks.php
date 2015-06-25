a9c5ec0acb0903294c7ebbd51988dbe794747536
Toine Kamps <t.kamps@dd-tkamps.local>
2015-06-25 11:11:19 +0200

    Add payment method to admin email

--- lib/woocommerce-hooks.php
+++ lib/woocommerce-hooks.php
@@ -185,3 +185,11 @@ function frontend_scripts_include_lightbox() {
     wp_enqueue_style( 'woocommerce_prettyPhoto_css', $woocommerce->plugin_url() . '/assets/css/prettyPhoto.css' );
   }
 }
+
+// Add Payment Type to WooCommerce Admin Email
+add_action( 'woocommerce_email_after_order_table', 'add_payment_method_to_admin_new_order', 15, 2 ); 
+function add_payment_method_to_admin_new_order( $order, $is_admin_email ) {
+  if ( $is_admin_email ) {
+    echo '<p><strong>Payment Method:</strong> ' . $order->payment_method_title . '</p>';
+  }
+}
