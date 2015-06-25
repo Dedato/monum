9b01e0a3445394f90a00406c4623d53429cc8707
Toine Kamps <t.kamps@dd-tkamps.local>
2015-06-25 11:10:58 +0200

    Made featured slider responsive and retina ready

--- includes/featured-slider.php
+++ includes/featured-slider.php
@@ -28,10 +28,10 @@ $query_args = array(
 /* Retrieve the slides, based on the query arguments. */
 $slides = woo_featured_slider_get_slides( $query_args );
 
-/* Media settings */
-$media_width = apply_filters( 'peddlar_media_width', '1024' );
-$media_height = apply_filters( 'peddlar_media_height', '768' );
-$media_settings = array( 'width' => $media_width, 'height' => $media_height );
+/* Media settings Large */
+$media_width     = apply_filters( 'peddlar_media_width', '1680' );
+$media_height    = apply_filters( 'peddlar_media_height', '670' );
+$media_settings  = array( 'width' => $media_width, 'height' => $media_height );
 
 if ( 'true' != $settings['featured_videotitle'] ) {
 	$media_settings['width'] = $media_width;
@@ -57,12 +57,11 @@ if ( false != $slides ) {
 		setup_postdata( $post );
 		$count++;
 		
-		$urnimage = get_field('slide_urn_image', get_the_ID() );
 		$url = get_post_meta( get_the_ID(), 'url', true );
 		$layout = get_post_meta( get_the_ID(), '_layout', true );
 		$title = get_the_title();
 		if ( $title != '' && $url != '' ) {
-			$title = '<a href="' . esc_url( $url ) . '" title="' . esc_attr( $title ) . '">' . $title . '</a>';
+			//$title = '<a href="' . esc_url( $url ) . '" title="' . esc_attr( $title ) . '">' . $title . '</a>';
 		}
 
 		$css_class = 'slide-number-' . esc_attr( $count );
@@ -73,10 +72,36 @@ if ( false != $slides ) {
 			$css_class .= ' has-video';
 			$slide_media = $embed;
 		} else {
-			$image = woo_image( 'width=' . $media_width . '&noheight=true&class=slide-image&link=img&return=true' );
+			$image       = woo_image( 'width=' . $media_width . '&noheight=true&class=slide-image&link=img&return=true' );
+      $img_alt     = get_post_meta( get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true);
+			$img_lg      = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'slider_large', true);
+			$img_md      = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'slider_medium', true);
+			$img_sm      = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'slider_small', true);
+      $img_lg_src  = $img_lg[0];
+      $img_md_src  = $img_md[0];
+			$img_sm_src  = $img_sm[0];
+			// Retina Images
+  		if (function_exists('wr2x_get_retina_from_url')) {
+  			$img_sm_2x_src 	= wr2x_get_retina_from_url($img_sm_src);
+  			$img_md_2x_src 	= wr2x_get_retina_from_url($img_md_src);
+  			$img_lg_2x_src 	= wr2x_get_retina_from_url($img_lg_src);
+  		}
 			if ( '' != $image ) {
 				$css_class .= ' has-image no-video';
 				$slide_media = $image;
+				ob_start();
+				  if ($url != '') { ?><a href="<?php echo esc_url($url); ?>" title="<?php echo esc_attr($title); ?>"><?php } ?>
+          <picture>
+        		<!--[if IE 9]><video style="display: none;"><![endif]-->
+        		<source srcset="<?php if ($img_lg_2x_src) { echo $img_lg_2x_src . ' 2x, '; } echo $img_lg_src .' 1x'; ?>" media="(min-width:1024px)">
+            <source srcset="<?php if ($img_md_2x_src) { echo $img_md_2x_src . ' 2x, '; } echo $img_md_src .' 1x'; ?>" media="(min-width:768px)">
+            <source srcset="<?php if ($img_sm_2x_src) { echo $img_sm_2x_src . ' 2x, '; } echo $img_sm_src .' 1x'; ?>">
+        		<!--[if IE 9]></video><![endif]-->
+        		<img class="<?php echo $css_class; ?>" srcset="<?php if ($img_md_2x_src) { echo $img_md_2x_src . ' 2x, '; } echo $img_md_src .' 1x'; ?>" alt="<?php echo $img_alt; ?>" />
+        	</picture>
+        	<?php if ($url != '') { ?></a><?php } ?>
+        <?php 
+        $slide_media = ob_get_clean();
 			} else {
 				$css_class .= ' no-image';
 			}
@@ -88,7 +113,7 @@ if ( false != $slides ) {
 		<li class="slide <?php echo esc_attr( $css_class ); ?>">
 			<?php
 				if ( '' != $slide_media ) {
-					echo '<div class="slide-media">' . $slide_media . '</div><!--/.slide-media-->' . "\n";
+					echo '<div class="slide-media">' . $slide_media . '</div>' . "\n";
 				}
 			?>
 			<?php if ( '' == $embed || ( '' != $embed && 'true' == $settings['featured_videotitle'] ) ) { ?>
@@ -102,11 +127,6 @@ if ( false != $slides ) {
   				<?php } ?>
   			</div><!--/.slide-content-->
 			</div>
-			<?php if ($urnimage) { ?>
-				<div class="slide-featured">
-					<img src="<?php echo $urnimage['sizes']['medium']; ?>" alt="<?php echo $urnimage['alt']; ?>" />
-				</div>
-			<?php } ?>
 			
 			<?php } ?>
 		</li>
