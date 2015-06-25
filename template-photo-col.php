6bf17dd1d8fd7a6d1ab27f94091bc0293aa49c22
Toine Kamps <t.kamps@dd-tkamps.local>
2015-06-25 11:11:44 +0200

    Switched from ACF gallery to repeater field

--- template-photo-col.php
+++ template-photo-col.php
@@ -6,28 +6,29 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  
 get_header();
 global $woo_options;
-$images     = get_field('left_photo_gallery');
 $main_class = 'no-gallery col-left';
 ?> 
   <div id="content" class="photo-page col-full">
   	<?php woo_main_before(); ?>
-  	<?php if ($images) { 
+  	<?php if( have_rows('photo_gallery') ): 
     	$main_class = 'col-right'; ?>
-      <section id="photo-gallery" class="col-left">
+    	<section id="photo-gallery" class="col-left">
         <ul class="images">
-          <?php foreach( $images as $image ): ?>
+          <?php while( have_rows('photo_gallery') ): the_row();
+        		$image   = get_sub_field('photo_gallery_image'); 
+        		$caption = get_sub_field('photo_gallery_caption'); ?>
             <li>
               <div class="image">
                 <a class="zoom" itemprop="image" title="<?php echo $image['title']; ?>" href="<?php echo $image['sizes']['large']; ?>" data-rel="prettyPhoto[page-gallery]">
-                  <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+                  <img src="<?php echo $image['sizes']['shop_catalog']; ?>" alt="<?php echo $image['alt']; ?>" />
                 </a>
               </div>  
-              <p><?php echo $image['caption']; ?></p>
+              <p class="caption"><?php echo $caption; ?></p>
             </li>
-          <?php endforeach; ?>
+          <?php endwhile; ?>
         </ul>
       </section>
-  	<?php } ?>
+    <?php endif; ?>
 		<section id="main" class="<?php echo $main_class; ?>">
       <?php if ( have_posts() ) { $count = 0;
         while ( have_posts() ) { the_post(); $count++; ?>                                                             
