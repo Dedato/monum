b3c935ab2cc74aaa2f9f1cd0e4cb627b0cfde7d2
Toine Kamps <t.kamps@dd-tkamps.local>
2015-07-02 17:34:21 +0200

    Changed 404 text

--- 404.php
+++ 404.php
@@ -13,10 +13,10 @@ get_header();
             <div class="page">
 				
 				<header>
-                	<h1><?php _e( 'Error 404 - Page not found!', 'woothemes' ); ?></h1>
+                	<h1><?php _e( 'Page not found', 'monum' ); ?></h1>
                 </header>
                 <section class="entry">
-                	<p><?php _e( 'The page you trying to reach does not exist, or has been moved. Please use the menus or the search box to find what you are looking for.', 'woothemes' ); ?></p>
+                	<p><?php _e( 'The page you trying to reach does not exist, or has been moved. Please use the menus or the search box to find what you are looking for.', 'monum' ); ?></p>
                 </section>
 
             </div><!-- /.post -->
