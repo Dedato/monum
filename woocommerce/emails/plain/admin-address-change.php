00a9087c33b8d02e47bb5aba95b1d91113a70533
Toine Kamps <t.kamps@dd-tkamps.local>
2015-07-08 16:21:35 +0200

    Plain text email template formatting

--- woocommerce/emails/plain/admin-address-change.php
+++ woocommerce/emails/plain/admin-address-change.php
@@ -11,36 +11,53 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+// Get current user
+global $woocommerce;
+global $current_user;
+get_currentuserinfo();
+$countries = new WC_Countries;
+
+
 echo "= " . $email_heading . " =\n\n";
 
-echo sprintf( __( 'You have received an order from %s.', 'woocommerce' ), $order->billing_first_name . ' ' . $order->billing_last_name ) . "\n\n";
+echo sprintf( __( '%s just updated his billing and/or shipping address:', 'monum' ), $current_user->user_firstname . ' ' . $current_user->user_lastname ) . "\n\n";
 
+echo __( 'Customer details', 'woocommerce' ) . "\n";
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+echo __('Username','woocommerce') . ": " . $current_user->user_login . "\n";
+echo __('Email','woocommerce') . ": " . $current_user->user_email . "\n";
+echo __('First name','woocommerce') . ": " . $current_user->user_firstname . "\n";
+echo __('Last name','woocommerce') . ": " . $current_user->user_lastname . "\n";
+
+echo __( 'Billing Address', 'woocommerce' ) . "\n";
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
-do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text );
+$billing_country      = get_user_meta( $current_user->ID, 'billing_country', true );
+$billing_country_full = ( $billing_country && isset( $countries->countries[ $billing_country ] ) ) ? $countries->countries[ $billing_country ] : $billing_country;
 
-echo strtoupper( sprintf( __( 'Order number: %s', 'woocommerce' ), $order->get_order_number() ) ) . "\n";
-echo date_i18n( __( 'jS F Y', 'woocommerce' ), strtotime( $order->order_date ) ) . "\n";
+echo __('Country','woocommerce'). ": " . $billing_country_full . "\n";
+echo __('First name or initials','monum'). ": " . get_user_meta( $current_user->ID, 'billing_first_name', true ) ."\n";
+echo __('Last name','woocommerce') . ": " . get_user_meta( $current_user->ID, 'billing_last_name', true ) . "\n";
+echo __('Company Name','woocommerce') . ": " .  get_user_meta( $current_user->ID, 'billing_company', true ) . "\n";
+echo __('Address','woocommerce') . ": " . get_user_meta( $current_user->ID, 'billing_address_1', true ) . "\n";
+echo __('Postcode','woocommerce') . ": " . get_user_meta( $current_user->ID, 'billing_postcode', true ) . "\n";
+echo __('City','woocommerce') . ": " . get_user_meta( $current_user->ID, 'billing_city', true ) . "\n";
+echo __('Email','woocommerce') . ": " . get_user_meta( $current_user->ID, 'billing_email', true ) . "\n";
+echo __('Telephone','woocommerce') . ": " . get_user_meta( $current_user->ID, 'billing_phone', true ) . "\n";
 
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text );
+echo __( 'Shipping Address', 'woocommerce' ) . "\n";
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
-echo "\n" . $order->email_order_items_table( false, true, '', '', '', true );
+$shipping_country      = $woocommerce->customer->get_shipping_country();
+$shipping_country_full = ( $shipping_country && isset( $countries->countries[ $shipping_country ] ) ) ? $countries->countries[ $shipping_country ] : $shipping_country;
 
-echo "==========\n\n";
-
-if ( $totals = $order->get_order_item_totals() ) {
-	foreach ( $totals as $total ) {
-		echo $total['label'] . " " . $total['value'] . "\n";
-	}
-}
-
-echo "\n" . sprintf( __( 'View order: %s', 'woocommerce'), admin_url( 'post.php?post=' . $order->id . '&action=edit' ) ) . "\n";
-
-echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
-
-do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text );
-
-do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text );
+echo __('Country','woocommerce'). ": " . $shipping_country_full . "\n";
+echo __('First name or initials','monum'). ": " . get_user_meta( $current_user->ID, 'shipping_first_name', true ) ."\n";
+echo __('Last name','woocommerce') . ": " . get_user_meta( $current_user->ID, 'shipping_last_name', true ) . "\n";
+echo __('Company Name','woocommerce') . ": " . get_user_meta( $current_user->ID, 'shipping_company', true ) . "\n";
+echo __('Address','woocommerce') . ": " . get_user_meta( $current_user->ID, 'shipping_address_1', true ) . "\n";
+echo __('Postcode','woocommerce') . ": " . get_user_meta( $current_user->ID, 'shipping_postcode', true ) . "\n";
+echo __('City','woocommerce') . ": " . get_user_meta( $current_user->ID, 'shipping_city', true ) . "\n";
 
 echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
