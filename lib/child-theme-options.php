2a01efbdfcbef97a64253b0fdbfa98b145830cc3
Toine Kamps <t.kamps@dd-tkamps.local>
2015-07-14 17:26:18 +0200

    Check if there is a more tag before splitting

--- lib/child-theme-options.php
+++ lib/child-theme-options.php
@@ -32,14 +32,17 @@ add_filter( 'jpeg_quality', create_function( '', 'return 100;' ) );
    
 // Split content at the more tag and return an array
 function split_content() {
-  global $more;
-  $more = true;
-  $content = preg_split('/<span id="more-\d+"><\/span>/i', get_the_content('more'));
-  // first content section in column1
-  $ret = '<div class="content_excerpt">'. array_shift($content). ' <a class="read-more" title="'. __('Read more', 'monum') .'" data-text-less="'. __('Read less', 'monum') .'">'. __('Read more', 'monum') .'</a></div>';
-  // remaining content sections in column2
-  if (!empty($content)) $ret .= '<div class="content_more">'. implode($content). '</div>';
-  return apply_filters('the_content', $ret);
+  global $post;
+  if( strpos( $post->post_content, '<!--more-->' ) ) {
+    $content = preg_split('/<span id="more-\d+"><\/span>/i', get_the_content('more'));
+    // first content section in column1
+    $ret = '<div class="content_excerpt">'. array_shift($content). ' <a class="read-more" title="'. __('Read more', 'monum') .'" data-text-less="'. __('Read less', 'monum') .'">'. __('Read more', 'monum') .'</a></div>';
+    // remaining content sections in column2
+    if (!empty($content)) $ret .= '<div class="content_more">'. implode($content). '</div>';
+    return apply_filters('the_content', $ret);
+  } else {
+    return the_content();
+  }  
 }
 
 
