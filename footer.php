aee97c57a29f8e1abcc8e5ea790ae92a10487546
Twansparant <toinekamps@gmail.com>
2016-03-24 11:52:38 +0100

    Cleanup & add language switcher

--- footer.php
+++ footer.php
@@ -31,52 +31,36 @@ if ( is_home() && 'true' == $settings['homepage_enable_promo_message'] ) {
 }
 ?>
 
-<?php
-if ( ( woo_active_sidebar( 'footer-1' ) ||
-		   woo_active_sidebar( 'footer-2' ) ||
-		   woo_active_sidebar( 'footer-3' ) ||
-		   woo_active_sidebar( 'footer-4' ) ) && $total > 0 ) {
-?>
-
-
-
-
-<div class="wrapper">
-
-	<?php woo_footer_before(); ?>
-
-	<section id="footer-widgets" class="col-full col-<?php echo $total; ?> fix">
-
-		<div class="fullwidth-widgets">
-
-			<?php woo_sidebar( 'footer-fullwidth' ); ?>
-
-		</div><!--/.fullwidth-widgets-->
-
-		<?php $i = 0; while ( $i < $total ) { $i++; ?>
-			<?php if ( woo_active_sidebar( 'footer-' . $i ) ) { ?>
-
-		<div class="block footer-widget-<?php echo $i; ?>">
-        	<?php woo_sidebar( 'footer-' . $i ); ?>
-		</div>
-
-	        <?php } ?>
-		<?php } // End WHILE Loop ?>
-
-	</section><!-- /#footer-widgets  -->
-<?php } // End IF Statement ?>
-
+<div class="content-info wrapper">
+  <?php if ( ( woo_active_sidebar( 'footer-1' ) ||
+  	   woo_active_sidebar( 'footer-2' ) ||
+  	   woo_active_sidebar( 'footer-3' ) ||
+  	   woo_active_sidebar( 'footer-4' ) ) && $total > 0 ) { ?>
+  	<?php woo_footer_before(); ?>
+  	<section id="footer-widgets" class="col-full col-<?php echo $total; ?> fix">
+  		<div class="fullwidth-widgets">
+  			<?php woo_sidebar( 'footer-fullwidth' ); ?>
+  		</div>
+  		<?php $i = 0; while ( $i < $total ) { $i++; ?>
+  			<?php if ( woo_active_sidebar( 'footer-' . $i ) ) { ?>
+      		<div class="block footer-widget-<?php echo $i; ?>">
+            <?php woo_sidebar( 'footer-' . $i ); ?>
+      		</div>
+        <?php } ?>
+  		<?php } ?>
+  	</section>
+  <?php } ?>
+  <?php do_action('wpml_add_language_selector'); ?>
 </div><!-- /.wrapper -->
+
 <footer id="footer" class="col-full">
-
 	<div class="footer-inner">
-
 		<div id="copyright" class="col-left">
-		<?php if( isset( $woo_options['woo_footer_left'] ) && $woo_options['woo_footer_left'] == 'true' ) {
-				echo stripslashes( $woo_options['woo_footer_left_text'] );
-		} else { ?>
-			<p><?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?>. <?php _e( 'All Rights Reserved.', 'woothemes' ); ?></p>
-		<?php } ?>
+  		<?php if( isset( $woo_options['woo_footer_left'] ) && $woo_options['woo_footer_left'] == 'true' ) {
+  				echo stripslashes( $woo_options['woo_footer_left_text'] );
+  		} else { ?>
+  			<p><?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?>. <?php _e( 'All Rights Reserved.', 'woothemes' ); ?></p>
+  		<?php } ?>
 		</div>
 		<div id="credit" class="col-right">
 	    <?php if( isset( $woo_options['woo_footer_right'] ) && $woo_options['woo_footer_right'] == 'true' ) {
@@ -89,9 +73,7 @@ if ( ( woo_active_sidebar( 'footer-1' ) ||
 		  </ul>
 		<?php } ?>
 		</div>
-
 	</div><!--/.footer-inner-->
-
 </footer><!-- /#footer  -->
 <?php wp_footer(); ?>
 <?php woo_foot(); ?>
